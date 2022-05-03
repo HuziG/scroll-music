4c1b9099d92c3fdad349cf8e6fb911d553e95cde
HuziG <1067408814@qq.com>
2022-05-03 18:20:25 +0800

    change

--- src/modules/permission.ts
+++ src/modules/permission.ts
@@ -7,12 +7,12 @@ export const install: UserModule = ({ router }) => {
   const whiteRoute = ['/login']
 
   router.beforeEach(async (to: any, from, next) => {
+    console.log('route')
+
     const data = await BaaS.auth.getCurrentUser()
 
     if (whiteRoute.includes(to.fullPath)) {
       if (data._email_verified) {
-        // @ts-ignore
-        alert('账号已登录')
         next('/')
       } else {
         next()
@@ -21,16 +21,6 @@ export const install: UserModule = ({ router }) => {
       if (data && data._email_verified) {
         next()
       } else {
-        if (!data) {
-          // @ts-ignore
-          alert('账号未登录！')
-        }
-
-        if (!data._email_verified) {
-          // @ts-ignore
-          alert('账号未进行邮箱验证，请登录邮箱验证！')
-        }
-
         next('/login')
       }
     }
