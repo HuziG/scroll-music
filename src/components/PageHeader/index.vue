a77190cfeb199d52c5aa1ccf8fc322c49eccb6bc
xiaoxuanzhang <1067408814@qq.com>
2023-08-13 09:41:25 +0800

    add some

--- src/components/PageHeader/index.vue
+++ src/components/PageHeader/index.vue
@@ -1,34 +1,53 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-import { userLogout } from '~/api/user'
-import { useUserStore } from '~/stores/user'
+import { useRouter } from 'vue-router';
+import { userLogout } from '~/api/user';
+import { useUserStore } from '~/stores/user';
 
-const value = ref('')
-const router = useRouter()
-const emit = defineEmits(['showHelp'])
-const iconList = ref([
-  { label: '关于作者', icon: 'i-mdi:book', value: 'about' },
-])
-const userStore = useUserStore()
+const value = ref('');
+const router = useRouter();
+const emit = defineEmits(['showHelp']);
+const iconList = ref([{ label: '关于作者', icon: 'i-mdi:book', value: 'about' }]);
+const userStore = useUserStore();
 
-const handleExit = async() => {
-  await userLogout()
-  window.location.reload()
-}
+const handleExit = async () => {
+  await userLogout();
+  window.location.reload();
+};
 </script>
 
 <template>
-  <div fixed top-0 left-0 box-border w-full z-30 shadow-md bg-white border-box h-1xl py-3 px-7 flex items-center justify-between>
-    <span text-xl font-bold border-b-2 border-primary>
-      滚动的曲谱
-    </span>
+  <div
+    fixed
+    top-0
+    left-0
+    box-border
+    w-full
+    z-30
+    shadow-md
+    bg-white
+    border-box
+    h-1xl
+    py-3
+    px-7
+    flex
+    items-center
+    justify-between
+  >
+    <span text-xl font-bold border-b-2 border-primary> 滚动的曲谱 </span>
 
     <div flex>
       <n-tooltip placement="bottom">
         <template #trigger>
           <div
-            i-mdi:progress-question flex items-center justify-center text-xl text-black
-            mr-12 cursor-pointer hover:opacity-60
+            i-mdi:progress-question
+            flex
+            items-center
+            justify-center
+            text-xl
+            text-black
+            mr-12
+            cursor-pointer
+            hover:opacity-60
             @click="emit('showHelp')"
           />
         </template>
@@ -38,16 +57,31 @@ const handleExit = async() => {
       <n-popover :overlap="overlap" placement="bottom-end" trigger="hover">
         <template #trigger>
           <div
-            i-mdi-account flex items-center justify-center text-xl text-black mr-3
-            cursor-pointer hover:opacity-60
+            v-if="!userStore.demoUser"
+            i-mdi-account
+            flex
+            items-center
+            justify-center
+            text-xl
+            text-black
+            mr-3
+            cursor-pointer
+            hover:opacity-60
           />
+          <div v-else></div>
         </template>
-        <div style="width: 300px;">
+        <div style="width: 300px">
           <div flex pt-2>
             <div
               v-for="item in iconList"
               :key="item.value"
-              mr-5 hover:text-primary cursor-pointer flex flex-col items-center justify-center
+              mr-5
+              hover:text-primary
+              cursor-pointer
+              flex
+              flex-col
+              items-center
+              justify-center
               @click="router.push('/about')"
             >
               <div bg-primary w-8 h-8 rounded-full flex items-center justify-center>
@@ -57,15 +91,11 @@ const handleExit = async() => {
             </div>
           </div>
 
-          <n-button mt-3 type="error" style="width: 100%;" @click="handleExit">
-            退出账号
-          </n-button>
+          <n-button mt-3 type="error" style="width: 100%" @click="handleExit"> 退出账号 </n-button>
         </div>
       </n-popover>
     </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/composables/baas.ts
+++ src/composables/baas.ts
@@ -1,7 +1,7 @@
-const BaaS = (window as any).BaaS
+const BaaS = (window as any).BaaS;
 
-const CLIENT_ID = (window as any).BAAS_KEY
+const CLIENT_ID = 'f5e845f48992981c90cc';
 
 export const installBaas = () => {
-  BaaS.init(CLIENT_ID, {})
-}
+  BaaS.init(CLIENT_ID, {});
+};
